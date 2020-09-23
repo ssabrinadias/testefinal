196bf253035392a130103c471adff85a49416734
Faiton <franklin.veiga@processmind.com.br>
2020-09-22 21:08:00 -0300

    inicial user card

--- app/src/Components/UserCard/UserCard.js
+++ app/src/Components/UserCard/UserCard.js
@@ -1,9 +1,19 @@
 import React from 'react'
 
-export default function UserCard() {
+export default function UserCard(props) {
+  const { username } = props
+  
   return (
-    <div>
-      
+    <div class="sidebar__profile">
+      <p class="sidebar__profile-name">
+      Olá, <span>{ username }</span>
+      </p>
+      <img class="sidebar__profile-icon" src="./img/icon__user.svg" alt="Icone de usuário">
+      <a href="#" class="sidebar__profile-link">
+        <img src="./img/icon__link.svg" alt="Ícone de link externo">
+      <span>Perfil completo</span>
+      </a>
     </div>
-  )
-}
+    )
+  }
+  
